3b438971a0a7c286936cebdbd2d195088a500d16
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-05-15 16:11:34 -0300

    Adiciona constraints na ActionView

--- AppMusic/CustomView/CardActionView.swift
+++ AppMusic/CustomView/CardActionView.swift
@@ -10,7 +10,6 @@ import UIKit
 class CardActionView: UIView {
 	
 	// MARK: - Components
-	
 	lazy var stackView: UIStackView = {
 		let sv = UIStackView()
 		sv.translatesAutoresizingMaskIntoConstraints = false
@@ -133,8 +132,47 @@ class CardActionView: UIView {
 		self.stackView.pin(to: self)
 		
 		NSLayoutConstraint.activate([
+			self.downloadButton.centerXAnchor.constraint(equalTo: self.downloadView.centerXAnchor),
+			self.downloadButton.centerYAnchor.constraint(equalTo: self.downloadView.centerYAnchor),
+			self.downloadButton.widthAnchor.constraint(equalToConstant: 45),
+			self.downloadButton.heightAnchor.constraint(equalToConstant: 45),
 			
+			self.notInterestedButton.centerXAnchor.constraint(equalTo: self.notInterestedView.centerXAnchor),
+			self.notInterestedButton.centerYAnchor.constraint(equalTo: self.notInterestedView.centerYAnchor),
+			self.notInterestedButton.widthAnchor.constraint(equalToConstant: 45),
+			self.notInterestedButton.heightAnchor.constraint(equalToConstant: 45),
+			
+			self.playButton.centerXAnchor.constraint(equalTo: self.playView.centerXAnchor),
+			self.playButton.centerYAnchor.constraint(equalTo: self.playView.centerYAnchor),
+			self.playButton.widthAnchor.constraint(equalToConstant: 70),
+			self.playButton.heightAnchor.constraint(equalToConstant: 70),
+			
+			self.likeButton.centerXAnchor.constraint(equalTo: self.likeView.centerXAnchor),
+			self.likeButton.centerYAnchor.constraint(equalTo: self.likeView.centerYAnchor),
+			self.likeButton.widthAnchor.constraint(equalToConstant: 45),
+			self.likeButton.heightAnchor.constraint(equalToConstant: 45),
+			
+			self.moreButton.centerXAnchor.constraint(equalTo: self.moreView.centerXAnchor),
+			self.moreButton.centerYAnchor.constraint(equalTo: self.moreView.centerYAnchor),
+			self.moreButton.widthAnchor.constraint(equalToConstant: 45),
+			self.moreButton.heightAnchor.constraint(equalToConstant: 45),
 		])
 	}
 	
+	public func updateLayout(for mode: ViewMode) {
+		if mode == .full {
+			self.downloadView.isHidden = false
+			self.moreView.isHidden = false
+			self.stackView.addArrangedSubview(self.downloadView)
+			self.stackView.addArrangedSubview(self.notInterestedView)
+			self.stackView.addArrangedSubview(self.playView)
+			self.stackView.addArrangedSubview(self.likeView)
+			self.stackView.addArrangedSubview(self.moreView)
+		} else {
+			self.downloadView.isHidden = true
+			self.moreView.isHidden = true
+			self.stackView.removeArrangedSubview(self.downloadView)
+			self.stackView.removeArrangedSubview(self.moreView)
+		}
+	}
 }
